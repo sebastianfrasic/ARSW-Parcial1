fa15442fef51d5197220ae4ef03b4cbfa495f93f
Juan Sebastián Frásica G <32890741+sebastianfrasic@users.noreply.github.com>
2020-09-10 12:48:06 -0500

    Punto 2

--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,34 +3,44 @@ package edu.eci.arsw.primefinder;
 import edu.eci.arsw.math.MathUtilities;
 
 import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrimeFinderThread extends Thread {
 
     private BigInteger a;
     private BigInteger b;
     private PrimesResultSet prs;
+    private AtomicInteger num;
 
-
-    public PrimeFinderThread(BigInteger a, BigInteger b, PrimesResultSet prs) {
+    public PrimeFinderThread(BigInteger a, BigInteger b, PrimesResultSet prs, AtomicInteger num) {
         this.a = a;
         this.b = b;
         this.prs = prs;
+        this.num = num;
     }
-
 
     @Override
     public void run() {
         MathUtilities mt = new MathUtilities();
-        int itCount = 0;
         BigInteger i = a;
         while (i.compareTo(b) <= 0) {
-            itCount++;
+            synchronized (this) {
+                if (PrimesFinderTool.isPaused()) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
             if (mt.isPrime(i)) {
                 prs.addPrime(i);
             }
             i = i.add(BigInteger.ONE);
         }
+        num.decrementAndGet();
     }
-
+    
 
 }
--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
@@ -1,55 +1,45 @@
 package edu.eci.arsw.primefinder;
 
+import edu.eci.arsw.mouseutils.MouseMovementMonitor;
+
 import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrimesFinderTool {
 
+    private static boolean paused;
+
     public static void main(String[] args) {
 
-        int maxPrim = 100;
+        int maxPrim = 100000;
         int numberOfThreads = 4;
 
         PrimesResultSet prs = new PrimesResultSet("john");
         PrimeFinderThread[] threads = new PrimeFinderThread[numberOfThreads];
 
+        //Contador global de threads corriendo
+        AtomicInteger num = new AtomicInteger(numberOfThreads);
+
         int cantidad = maxPrim / numberOfThreads;
         int inicio = 0;
 
         for (int i = 0; i < numberOfThreads; i++) {
-            threads[i] = new PrimeFinderThread(new BigInteger(Integer.toString(inicio)), new BigInteger(Integer.toString(inicio + cantidad)), prs);
+            threads[i] = new PrimeFinderThread(new BigInteger(Integer.toString(inicio)), new BigInteger(Integer.toString(inicio + cantidad)), prs, num);
             threads[i].start();
             inicio += cantidad;
         }
 
-        for (int i = 0; i < numberOfThreads; i++) {
-            try {
-                threads[i].join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+        while (num.get() > 0) {
+            paused = MouseMovementMonitor.getInstance().getTimeSinceLastMouseMovement() >= 10000;
         }
 
         System.out.println("Prime numbers found:");
         System.out.println(prs.getPrimes());
-            
-            
-            /*while(task_not_finished){
-                try {
-                    //check every 10ms if the idle status (10 seconds without mouse
-                    //activity) was reached. 
-                    Thread.sleep(10);
-                    if (MouseMovementMonitor.getInstance().getTimeSinceLastMouseMovement()>10000){
-                        System.out.println("Idle CPU ");
-                    }
-                    else{
-                        System.out.println("User working again!");
-                    }
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(PrimesFinderTool.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }*/
+    }
 
-
+    public static boolean isPaused() {
+        return paused;
     }
 
 }
